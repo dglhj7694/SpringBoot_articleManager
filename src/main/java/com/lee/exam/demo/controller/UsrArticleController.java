2c737eae032642ae9df2e821008a17ca3220c22f
hjlee <lee_hajin@naver.com>
2022-10-31 01:23:10 +0900

    getArticle, deleteArticle 메서드로 게시물 삭제 기능 구현

--- src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
@@ -15,12 +15,30 @@ public class UsrArticleController {
 	private int lastArticleId;
 	private List<Article> articles;
 
+	// 생성자
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
 
 		// test data
 		makeTestData();
+	}
+
+	// 서비스 메서드
+	// get article
+	private Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+
+	// 삭제
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		articles.remove(article);
 	}
 
 	private Article writeArticle(String title, String body) {
@@ -31,20 +49,6 @@ public class UsrArticleController {
 		return article;
 	}
 
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
-		Article article = writeArticle(title, body);
-		return article;
-	}
-
-	@RequestMapping("/usr/article/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-
-		return articles;
-	}
-
 	// make test data
 	private void makeTestData() {
 		for (int i = 0; i < 10; i++) {
@@ -53,6 +57,36 @@ public class UsrArticleController {
 
 			writeArticle(title, body);
 		}
+	}
+
+	// action method
+
+	// 게시물 작성
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
+		return article;
+	}
+
+	// 게시물 삭제
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
+		if (article == null) {
+			return id + "번 게시물은 없던데?";
+		}
+		deleteArticle(id);
+		return id + "번 게시물을 삭제했습니다.";
+	}
+
+	// 게시물 리스트 조회
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+
+		return articles;
 	}
 
 }
