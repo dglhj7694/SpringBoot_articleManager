435dbfe5cee43b4c75fd015394bfcfe598ff66d4
hjlee <lee_hajin@naver.com>
2022-10-30 16:46:20 +0900

    [test] count 컨트롤러 액션

--- src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
@@ -14,33 +14,17 @@ public class UsrHomeController {
 	//http://localhost:8081/usr/home/main
 	//application.yml .server.port 변경
 	
-	@RequestMapping("/usr/home/main")
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요";
-	}
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다";
-	}
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "잘가요";
+	public int getCount() {
+		return count;
 	}
 	
-	@RequestMapping("/usr/home/main4")
+//	http://localhost:8081/usr/home/doSetCount?count=10
+	@RequestMapping("/usr/home/doSetCount")
 	@ResponseBody
-	public int showMain4() {
-//		count++; //1부터 나오지 (전위 후위 연산자)
-		return count++; //0부터 나오고 1씩 증가
-	}
-	
-	@RequestMapping("/usr/home/main5")
-	@ResponseBody
-	public String showMain5() {
-		count = 0;
-		return "count를 다시 0으로 초기화 함"; 
-	}
+	public String doSetCount(int count) {
+		this.count = count;
+		return "count 의 값이 " + this.count + "로 초기화됨";
+		}
 }
