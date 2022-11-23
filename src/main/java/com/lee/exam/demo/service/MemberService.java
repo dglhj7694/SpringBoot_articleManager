ab3935d941fdd5d33dcaf7746bb25b05f18dd950
hjlee <lee_hajin@naver.com>
2022-11-24 01:33:05 +0900

    회원가입 준비, UsrMemberController,MemberService 추가

---
+++ src/main/java/com/lee/exam/demo/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.lee.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	//private MemberRepository memberRepository;
+
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+//		memberRepository.join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
+		
+	}
+
+}
